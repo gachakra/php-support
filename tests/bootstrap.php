e1a049f0a64439973fe7fb2ebc0da18247aab06d
Kangin Kim <gachakra.kangin@notonza.dev>
2025-03-27 16:52:51 +0900

    phpunit.xml, tests/bootstrap.php / psalm.xml

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gachakra
+ * Date: 2020/03/29
+ * Time: 17:18
+ */
+
+error_reporting(E_ALL | E_STRICT);
+require __DIR__ . '/../vendor/autoload.php';
